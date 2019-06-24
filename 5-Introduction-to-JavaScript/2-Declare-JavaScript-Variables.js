ea4906a579ffa02f441a0889cb9300431cbd3b67
Paige VanGieson <paigevg@gmail.com>
2019-06-24 15:58:12 -0400

    completed JS-2

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,13 @@
+/*
+Javascript has several different data types. Different kinds of operations are applicable to each. 
+
+Variables act as a label for data, and allow for dynamic data storage and manipulation.
+
+Variables are declared with the var keyword, and can contain any alphanumeric character as well as underscores and dollar signs. They may not start with a number.
+
+Statements are ended with semicolons.
+*/
+
+
+var ourName;
+var myName;
