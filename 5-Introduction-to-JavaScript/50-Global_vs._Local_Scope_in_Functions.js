0aa958a7c5825a1de88076a99bf7a102f250c450
Paige VanGieson <paigevg@gmail.com>
2019-06-25 12:27:31 -0400

    completed JS-50

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,11 @@
+// Local variables will override global variables of the same name.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater";
+  return outerWear;
+}
+
+myOutfit();
