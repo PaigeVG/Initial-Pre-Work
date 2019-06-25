367edca3e92197f278731617bdb03a05149a14ef
Paige VanGieson <paigevg@gmail.com>
2019-06-25 12:17:14 -0400

    completed JS-47

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+// Function parameters act as placeholders to be replaced with arguments when the function is called.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(n, m) {
+  console.log(n + m); 
+}
+
+functionWithArgs(7, 77);
