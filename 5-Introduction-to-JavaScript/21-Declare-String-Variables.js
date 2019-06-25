3d90dcc461029b730eb2a09f8ec949207ca89088
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:30:36 -0400

    completed JS-21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,7 @@
+// String literals represent a series of characters and are enclosed in quotes.
+
+var firstName = "Alan";
+var lastName = "Turing";
+
+var myFirstName = "Paige";
+var myLastName = "VanGieson";
