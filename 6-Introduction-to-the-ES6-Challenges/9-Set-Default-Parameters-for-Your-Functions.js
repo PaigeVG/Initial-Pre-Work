065f42b23c994af12f1ef068da9fd2dcf60e2b39
Paige VanGieson <paigevg@gmail.com>
2019-06-25 11:04:47 -0400

    completed ES6-9

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+// Use assignment inside function parameters to set a default value.
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
