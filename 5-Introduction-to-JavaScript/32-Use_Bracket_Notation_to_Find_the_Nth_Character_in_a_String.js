fb1daa10e6d0a1f8621ede9fcf372cbb91135c41
Paige VanGieson <paigevg@gmail.com>
2019-06-24 23:01:23 -0400

    completed JS-32

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// use bracket access to get at any character in a string
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
