518eb8d528773dd8885295700f6e5e898a92fe8a
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:51:50 -0400

    completed JS-28

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// += can be used with string variables
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "neat";
+var myStr = "Learning to code is ";
+myStr += someAdjective
