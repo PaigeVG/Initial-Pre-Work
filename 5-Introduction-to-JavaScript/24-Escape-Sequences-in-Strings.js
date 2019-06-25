024f451d0b12c2727b1126f7b2fe6fbec3dd97c1
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:39:21 -0400

    completed JS-24

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,4 @@
+// Several different characters, most notably different kinds of whitespace, are represented by a backslash and a letter. Backslashes themselves must be escaped in order to appear in the string.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
