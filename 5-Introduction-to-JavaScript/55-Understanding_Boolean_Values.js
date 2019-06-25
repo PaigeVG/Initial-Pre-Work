72f835220830d2a6210c6d037b56169db14e4b20
Paige VanGieson <paigevg@gmail.com>
2019-06-25 13:46:26 -0400

    completed JS-55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// Booleans are data types with only two possible values: true or false.
+
+function welcomeToBooleans() {
+
+  // Only change code below this line.
+  
+  return true; // Change this line
+  
+  // Only change code above this line.
+  }
