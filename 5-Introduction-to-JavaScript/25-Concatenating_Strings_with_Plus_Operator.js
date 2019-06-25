c5aa38b3a71cce867ecebb4c7dbbbf7a7d85f6f9
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:45:17 -0400

    completed JS-25

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// + becomes the concatenation operator when used with two strings and combines them into one. 
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
