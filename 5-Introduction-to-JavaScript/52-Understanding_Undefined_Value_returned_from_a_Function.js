162a913d23febfeeb15d323d05c82da4b955831f
Paige VanGieson <paigevg@gmail.com>
2019-06-25 12:34:38 -0400

    completed JS-52

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// A function without any explicit return will still execute its body, but will return undefined.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
