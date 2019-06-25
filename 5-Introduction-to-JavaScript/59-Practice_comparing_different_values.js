aa0ef917823320931cfc21db8c37101a3b096b01
Paige VanGieson <paigevg@gmail.com>
2019-06-25 14:00:20 -0400

    completed JS-59

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// reinforcing equality vs strict equality with some practice
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
