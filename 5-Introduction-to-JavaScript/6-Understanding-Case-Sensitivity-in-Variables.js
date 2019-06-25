ffed4e00ff26fa129f3d44d6c0d0759e8f4ce8fa
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:04:11 -0400

    completed JS-6

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,13 @@
+/*
+JavaScript is case sensitive
+
+camelCase is the standard convention for variable names
+*/
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
