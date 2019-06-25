82ceec4ee2c0fb9bafba7e0be6f3d30ddd101da4
Paige VanGieson <paigevg@gmail.com>
2019-06-25 10:26:04 -0400

    completed ES6-6

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,3 @@
+// Arrow syntax is great little piece of syntactic sugar for defining small, one-off anonymous functions that lets us skip a lot of boilerplate and thereby create more readable code.
+
+const magic = () => new Date();
