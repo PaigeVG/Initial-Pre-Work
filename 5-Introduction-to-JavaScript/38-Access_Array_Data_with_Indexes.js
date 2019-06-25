9744cabf2d2eed652203c52fa659e816d443975b
Paige VanGieson <paigevg@gmail.com>
2019-06-25 11:13:35 -0400

    completed JS-38

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,7 @@
+// Since arrays are just another value, they can be nested inside of other arrays.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["potato", "tomato", "elbow", "john doe"], ["yam", "Sam", "lamb", "spam"]];
