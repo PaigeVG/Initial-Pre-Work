4f610c5a51bbf592360aca7275e56f2d130f08f3
Paige VanGieson <paigevg@gmail.com>
2019-06-25 12:32:19 -0400

    completed JS-51

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+// return allows you to extract a value out of the function body.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(n) {
+  return n * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(7));
