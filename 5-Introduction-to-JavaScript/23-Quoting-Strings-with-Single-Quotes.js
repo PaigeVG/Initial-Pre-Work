e9de30f0ec7da0c2aa1948fa1a1c8bb3ab06899a
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:36:30 -0400

    completed JS-23

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+// Double and single quotes can both be used to make strings, and work identically. Sometimes it's convenient to use the opposite of what appears in the string to avoid having to escape quotemarks.
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
