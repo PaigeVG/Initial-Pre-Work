b91eb9da00d59d733f7966652c1af3e82a6b1f3a
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:01:53 -0400

    completed JS-5

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,13 @@
+/*
+Variables have the value undefined before assignment
+
+JavaScript will type coerce undefined to a string oh no
+*/
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
