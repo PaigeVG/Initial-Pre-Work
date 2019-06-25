3b9008eaff8dc1344709878d841d4eab11ff2226
Paige VanGieson <paigevg@gmail.com>
2019-06-25 12:13:49 -0400

    completed JS-46

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+// functions (reusable pieces of code) can be defined using the function keyword, and invoked via parens. The code inside their body will be executed when they are called.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
