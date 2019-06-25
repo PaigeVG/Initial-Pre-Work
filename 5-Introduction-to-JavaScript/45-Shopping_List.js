6da795be4a4e1f913d1518b7f75c637a3f3d2918
Paige VanGieson <paigevg@gmail.com>
2019-06-25 11:56:36 -0400

    completed JS-45

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,14 @@
+// unshift will add elements to the start of an array rather than remove them.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); 
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35])
