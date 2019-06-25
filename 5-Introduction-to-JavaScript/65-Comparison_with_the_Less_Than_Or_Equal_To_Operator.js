130656fdb38ce3005a9a2c86340733e44596078f
Paige VanGieson <paigevg@gmail.com>
2019-06-25 14:12:27 -0400

    completed JS-65

--- 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -1,1 +1,16 @@
-// Less than or equal to, >= , returns true if the rightmost value is less than or equal to the leftmost value, and false otherwise.
+// Less than or equal to, <= , returns true if the rightmost value is less than or equal to the leftmost value, and false otherwise.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+  
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
