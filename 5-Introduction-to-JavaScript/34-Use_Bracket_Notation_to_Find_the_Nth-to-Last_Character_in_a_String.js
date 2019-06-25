9a5c5c1d7ceb54585ef4039e1b2c9971ee516f6e
Paige VanGieson <paigevg@gmail.com>
2019-06-24 23:04:33 -0400

    completed JS-34

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// keep subtracting from .length to get further back in the string
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
