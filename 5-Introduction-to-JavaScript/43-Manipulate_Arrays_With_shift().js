aab8f26d494269649c685dc2f99211d8a25a58ea
Paige VanGieson <paigevg@gmail.com>
2019-06-25 11:39:55 -0400

    completed JS-43

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,13 @@
+// pop is a similar function, but takes an element OFF the end of an array instead and returns it.
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); 
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
+
