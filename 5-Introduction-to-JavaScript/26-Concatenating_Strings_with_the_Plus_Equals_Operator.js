604c9ae6b4b8b5ae80ae52b61ffa89739b71fae3
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:47:19 -0400

    completed JS-26

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+// += for strings is analagous to += for numbers. It takes the existing value of a string variable and concats whatever is to the right. 
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+var myStr = "This is the first sentence. "
+myStr += "This is the second sentence.";
