a46adef52050664267c8f78afa7efa558a633fd6
Paige VanGieson <paigevg@gmail.com>
2019-06-24 23:30:39 -0400

    completed ES6-2

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+// let will scope a variable to a block, statement or expression in addition to functions, while var only scopes to functions.
+
+function checkScope() {
+  "use strict";
+    let i = "function scope";
+    if (true) {
+      let i = "block scope";
+      console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+  }
