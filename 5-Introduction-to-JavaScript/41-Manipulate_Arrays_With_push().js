b579f4be1a97edc2baf8cdcbd8ab202b573665a7
Paige VanGieson <paigevg@gmail.com>
2019-06-25 11:33:39 -0400

    completed JS-41

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,7 @@
+// Once you access an array element with bracket notation, you have that element in hand as a value. So, if that value is another array that you want to access, you just pop another set of brackets on.
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
