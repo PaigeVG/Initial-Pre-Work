af3a0af1d790d387fde2b073b65d01eca3d70d68
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:56:14 -0400

    completed JS-29

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,16 @@
+// Find out the length of a string using its .length property
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
