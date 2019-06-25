94914ba4f3e2883ebc446dc29269e22da53a6c85
Paige VanGieson <paigevg@gmail.com>
2019-06-24 23:00:26 -0400

    completed JS-31

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// JS strings are immutable. This does not, of course, mean that variables can't be reassigned to different string literals, but you can't re-assign any of the characters making them up.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World";
+
