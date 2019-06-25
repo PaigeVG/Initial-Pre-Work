4a937b307ac7e2cd86478d760e508e6edd4b9562
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:50:15 -0400

    completed JS-27

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+// Concatenate string variables like any other string using + 
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Paige";
+var myStr = "My name is " + myName + " and I am well!";
+
