0ea765d5290514e95720a317ad774de6b2a0affd
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:14:43 -0400

    completed JS-16

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,3 @@
+// % is the remainder operator and does exactly what's on the tin. It's often used to check if numbers are even or odd. Modulus and remainder are not synonymous.
+
+var remainder = 11 % 3;
