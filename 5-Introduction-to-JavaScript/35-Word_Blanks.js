374a555504b2a1be1a2ef2236a64cfe99a6a80e0
Paige VanGieson <paigevg@gmail.com>
2019-06-24 23:10:04 -0400

    completed JS-35

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+// just an exercise to reinforce concepts taught in the past few lessons
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "Thanks to " + myNoun + " I was feeling " +myAdjective + " so I went to " +myVerb + " but ended up doing it very " + myAdverb + " indeed.";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
