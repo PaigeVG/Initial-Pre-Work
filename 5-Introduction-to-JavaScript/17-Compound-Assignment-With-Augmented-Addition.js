77a32dd76d361cdf37f3b703807f984dc4892ee4
Paige VanGieson <paigevg@gmail.com>
2019-06-24 22:20:02 -0400

    completed JS-17

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,14 @@
+/* 
++= operator provides a shorthand for things like foo = foo + 8 
+
+if you change freeCodeCamp's line numbers it will get angry
+*/
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
