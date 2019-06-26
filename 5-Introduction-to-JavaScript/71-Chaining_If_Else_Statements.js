0af728e1d57766b80c788e609923a40845e42e34
Paige VanGieson <paigevg@gmail.com>
2019-06-26 13:54:23 -0400

    completed JS-71

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,26 @@
+// else if statements can be chained to check multiple conditions.
+
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  }
+
+  else if (num < 10) {
+    return "Small";
+  }
+  
+  else if (num < 15) {
+    return "Medium";
+  }
+
+  else if (num < 20) {
+    return "Large";
+  } 
+
+  else {
+    return "Huge";
+  }
+}
+
+// Change this value to test
+testSize(7);
