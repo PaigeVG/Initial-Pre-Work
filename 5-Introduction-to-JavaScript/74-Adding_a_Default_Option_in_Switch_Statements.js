7b34a414155bb81142c80e2ec635eb3a3cabdb33
Paige VanGieson <paigevg@gmail.com>
2019-06-26 14:10:26 -0400

    completed JS-74, JS-75

---
+++ 5-Introduction-to-JavaScript/74-Adding_a_Default_Option_in_Switch_Statements.js
@@ -0,0 +1,27 @@
+// default statements are a bit like else for switch statements. They will evaluate their code block if no other conditional matches.
+
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+switchOfStuff(1);
