107f3ee7bcbc1823bf74eeb485720405ff69644a
Paige VanGieson <paigevg@gmail.com>
2019-06-26 13:47:38 -0400

    completed JS-68

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,20 @@
+// Use else statements to specify what to do when a set of conditionals are all false.
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+  
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  
+  else {
+    result = "5 or Smaller";
+  }
+  
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
