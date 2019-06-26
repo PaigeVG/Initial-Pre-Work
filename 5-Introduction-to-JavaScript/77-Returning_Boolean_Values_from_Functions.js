03f62fca8e68bd02597c0295a276cee35a6a80ba
Paige VanGieson <paigevg@gmail.com>
2019-06-26 14:52:27 -0400

    completed JS-77

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// conditionals are pointless if you just want to return the boolean result of some expression. Just return the bool.
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
