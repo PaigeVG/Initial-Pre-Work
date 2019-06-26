bf75eba9a5e451f2cfe53be224ffcda671f93ba6
Paige VanGieson <paigevg@gmail.com>
2019-06-26 14:11:58 -0400

    added breaks to JS-75 switch

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -9,14 +9,17 @@ function sequentialSizes(val) {
     case 2:
     case 3:
       return "Low";
+      break;
     case 4:
     case 5:
     case 6:
       return "Mid";
+      break;
     case 7:
     case 8:
     case 9:
       return "High";
+      break;
   }
   
   
