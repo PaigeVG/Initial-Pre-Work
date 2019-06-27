dd3e0cdd108923ddbab9417e267c1f0e4c586457
Paige VanGieson <paigevg@gmail.com>
2019-06-27 12:27:22 -0400

    completed JS-91

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,16 @@
+// chain dot or bracket notation to access nested objects. Mixing and matching is allowed.
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
