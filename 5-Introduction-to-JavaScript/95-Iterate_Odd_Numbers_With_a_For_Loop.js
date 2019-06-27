c34386bfc41f0b29c899b8105f1e25489a90e437
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:01:47 -0400

    completed JS-95

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,18 @@
+// a for loop runs some initialization once before the first loop, runs until its condition is false, and also executes an expression at the end of each iteration.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+
