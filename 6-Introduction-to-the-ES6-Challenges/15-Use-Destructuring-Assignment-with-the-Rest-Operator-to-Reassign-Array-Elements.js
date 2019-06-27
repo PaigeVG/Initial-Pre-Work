8b1aa0f8382a0536b5ddeaf445576925ddacf200
Paige VanGieson <paigevg@gmail.com>
2019-06-27 15:26:05 -0400

    completed ES6-15

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,13 @@
+// Assign the rest of the elements of an array you are destructuring to a seperate array using the spread operator.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  let [a, b, ...arr] = list;
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
