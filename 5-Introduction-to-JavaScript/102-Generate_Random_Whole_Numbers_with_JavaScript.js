d3b4d666a49a3da722dcaa2967cb0ccee4a4d6e9
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:29:25 -0400

    completed JS-102

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,7 @@
+// Generate whole numbers by multipying and calling Math.floor
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+  return   Math.floor(Math.random() * 10);
+}
