9df88bf2a7d5c3affa90e8511a12018c5e0139cc
Paige VanGieson <paigevg@gmail.com>
2019-06-27 14:47:42 -0400

    completed ES6-11

--- 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -1,10 +1,14 @@
-// Q: Why does Math.max.apply(null, someArray) work? If Math.max expects comma-seperated args, why would changing the calling context do anything?
+/* 
+Q: Why does Math.max.apply(null, someArray) work? If Math.max expects comma-seperated args, why would changing the calling context do anything?
+
+The spread operator will expand arrays and other expressions into places like function parameters and literals.
+*/
 
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
 (function() {
   "use strict";
-  arr2 = []; // change this line
+  arr2 = [...arr1]; // change this line
 })();
 console.log(arr2);
 
