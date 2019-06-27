87e74872e0ec88434c42e6b747adb1f7aa26add8
Paige VanGieson <paigevg@gmail.com>
2019-06-27 11:32:17 -0400

    completed JS-81

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,18 @@
+// Objects are a type of collection that stores structured data in properties, which are strings (or numbers, which are typecast as strings in the end).
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Galvantula",
+  "legs": 8,
+  "tails": 0,
+  "friends": ["me", "you", "pikachu"]
+};
