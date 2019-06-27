b9b8108216539afcdb94d7556c232e21389ee267
Paige VanGieson <paigevg@gmail.com>
2019-06-27 16:02:35 -0400

    completed JS-19

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+// when defining a function object property, you can omit the colon and replace the function keyword with its name.
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
