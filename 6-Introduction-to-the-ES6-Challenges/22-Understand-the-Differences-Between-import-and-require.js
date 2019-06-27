8d73fa26e98b48227e0df73cc878a270bad9087c
Paige VanGieson <paigevg@gmail.com>
2019-06-27 16:28:10 -0400

    completed ES6-22

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+// Import allows you to bring in only certain parts of some external module, unlike require which brings in the whole shebang.
+
+import { capitalizeString } from "string_functions";
+
+"use strict";
+capitalizeString("hello!");
