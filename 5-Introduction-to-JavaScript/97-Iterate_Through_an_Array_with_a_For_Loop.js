850225dd6aab1842eb6bca2ea54ad8a8504e9cc4
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:05:20 -0400

    completed JS-97

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,17 @@
+// You can count up (or down) your loop variable however you want
+
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
