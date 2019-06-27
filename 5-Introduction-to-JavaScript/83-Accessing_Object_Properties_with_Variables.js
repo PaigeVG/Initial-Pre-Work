907d4bc67c7d1310c8b36e0564238176263aea3b
Paige VanGieson <paigevg@gmail.com>
2019-06-27 11:38:55 -0400

    completed JS-83

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// You have to use bracket notation if the property has a space in its name, and the name must be in quotations.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
