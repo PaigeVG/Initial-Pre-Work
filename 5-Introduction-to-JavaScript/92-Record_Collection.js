c96e567178546565207fccdb88a982b3920db93c
Paige VanGieson <paigevg@gmail.com>
2019-06-27 12:31:43 -0400

    completed JS-92

---
+++ 5-Introduction-to-JavaScript/92-Record_Collection.js
@@ -0,0 +1,25 @@
+// You can nest objects in objects, arrays in objects, objects in arrays. Go nuts! Access will still work and chain just as you expect.
+
+// Setup
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
