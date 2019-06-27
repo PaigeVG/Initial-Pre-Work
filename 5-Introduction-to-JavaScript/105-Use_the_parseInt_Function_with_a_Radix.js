7dd7bbbc2eb447093d69810c57e3d9ebb3bc7593
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:53:19 -0400

    completed JS-105

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,7 @@
+// convert in different number bases using parseIn't second argument called the radix.
+
+function convertToInteger(str, radix=2) {
+  return parseInt(str, radix);
+}
+
+convertToInteger("10011");
