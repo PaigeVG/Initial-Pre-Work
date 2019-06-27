606ec8e32355f6dbaee0fc9bdf1956cbc55902c1
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:25:02 -0400

    fixed name of and completed JS-99

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,13 @@
+// do... while is a type of loop that will execute a piece of code under the "do" once before it launches into the "while". A normal while would not do anything if its condition evaluated to false on the first loop.
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
+
