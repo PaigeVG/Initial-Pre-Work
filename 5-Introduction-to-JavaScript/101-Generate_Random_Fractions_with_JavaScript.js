799ae7d56b6d4bb86c7caeca7b874fdb1d85f59b
Paige VanGieson <paigevg@gmail.com>
2019-06-27 13:27:35 -0400

    completed JS-101

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// Math.random will generate a decimal number in interval [0, 1)
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
